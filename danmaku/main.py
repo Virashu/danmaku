cbc88c769c285bfafa987ff5ccfee2310049c353
virashu <svladichek@gmail.com>
2024-01-27 18:26:08 +0300

    Remove debug prints

--- danmaku/main.py
+++ danmaku/main.py
@@ -192,10 +192,6 @@ class Game(vgame.Game):
         ...
 
 
-import os
-
-print(os.listdir(resource_path("./resources/")))
-
 pygame.mixer.init()
 pygame.mixer.music.set_volume(0.5)
 pygame.mixer.music.load(resource_path("./resources/sounds/bgm.wav"))
