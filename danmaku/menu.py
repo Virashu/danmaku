efc2069e59a5a1303eef22d42ed4952aa2ed8f72
Vlad <89295404+Virashu@users.noreply.github.com>
2024-02-03 01:21:50 +0300

    Update

--- danmaku/menu.py
+++ danmaku/menu.py
@@ -11,35 +11,12 @@ class Menu(vgame.Scene):
         self.last_game = False
         self.status = self.new_game or self.last_game
 
-        self.buttons = [
-            Button(
-                (self.width // 4, self.height // 3),
-                200,
-                40,
-                (255, 0, 0),
-                "New game",
-                (0, 0, 255),
-                42,
-            ),
-            Button(
-                (self.width // 4 + 10, self.height // 3 + 50),
-                200,
-                40,
-                (255, 0, 0),
-                "Continue",
-                (0, 0, 255),
-                42,
-            ),
-            Button(
-                (self.width // 4 + 13, self.height // 3 + 100),
-                200,
-                40,
-                (255, 0, 0),
-                "Settings",
-                (0, 0, 255),
-                42,
-            ),
-        ]
+        self.buttons = (
+            ("New game", "new_game"),
+            ("Continue", "continue"),
+            ("Settings", "settings"),
+            ("Quit", "quit"),
+        )
 
     def update(self):
         if vgame.Keys.UP in self.pressed_keys:
@@ -48,19 +25,27 @@ class Menu(vgame.Scene):
         if vgame.Keys.DOWN in self.pressed_keys:
             self.pressed_keys.discard(vgame.Keys.DOWN)
             self.selection_index = (self.selection_index + 1) % len(self.buttons)
-        if vgame.Keys.RETURN in self.pressed_keys or vgame.Keys.Z in self.pressed_keys:
-            if self.selection_index == 0:
-                self.new_game = True
-                # Delete game from db & go to game scene
-            if self.selection_index == 1:
-                self.last_game = True
-                self.stop()
-                # just go to game scene
-            if self.selection_index == 2:
-                pass
-            self.status = self.new_game or self.last_game
-        print(self.selection_index)
+        if {vgame.Keys.RETURN, vgame.Keys.Z, vgame.Keys.SPACE} & self.pressed_keys:
+            match self.buttons[self.selection_index][1]:
+                case "new_game":
+                    # Delete game from db & go to game scene
+                    ...
+                case "continue":
+                    # Load game from db & go to game scene
+                    ...
+                case "settings":
+                    # Go to settings scene
+                    ...
+                case "quit":
+                    # Trigger pygame quit event
+                    ...
 
     def draw(self):
-        for button in self.buttons:
-            button.draw(self.graphics)
+        for i, button in enumerate(self.buttons):
+            self.graphics.text(
+                button[0],
+                (0, i * 50),
+                (255, 200, 180) if i == self.selection_index else (255, 255, 255),
+            )
+
+    def exit(self): ...
